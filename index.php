5c04720ce6bc8cd96219556e18013187e0618f9d
pity <pity@gmail.com>
2026-08-26 10:13:32 +0700

    chore: initial project setup

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "Halo, Localhost dan PHP 8 sudah berhasil berjalan!";
-?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum Pemrograman Web</title>
+</head>
+<body>
+    <h1>Praktikum Pemrograman Web</h1>
+</body>
+</html>
